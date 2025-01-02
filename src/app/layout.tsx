7ca909690c7c04cd07dcc5f66ca02a6edd124779
mxrk-dev <damark215@gmail.com>
2025-01-02 14:27:11 +0100

    New

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import TransitionProvider from "@/components/transitionProvider";
+import Providers from "./providers";
 // import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
@@ -29,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <TransitionProvider>{children}</TransitionProvider>
+        <Providers><TransitionProvider>{children}</TransitionProvider></Providers>
       </body>
     </html>
   );
